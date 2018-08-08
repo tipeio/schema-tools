47c8f27f7a73fce46577d3339bc277ce9e1104e6
Scott Moss <willscottmoss@gmail.com>
2018-08-07 22:53:00 -0700

    :police_car: add specs

--- src/convert.js
+++ src/convert.js
@@ -1,113 +1,24 @@
 import scalars from './scalars'
-// import { directives } from './directives'
 import { makeExecutableSchema } from 'graphql-tools'
 import { schemaToTemplateContext } from 'graphql-codegen-core'
 import { parse, extendSchema } from 'graphql'
-
-const fs = require('fs')
-const path = require('path')
-const loc = path.join(__dirname, '../tipe.graphql')
-
-export const getSchemaTemplateData = () => {
-  let schema = fs.readFileSync(loc, { encoding: 'utf8' })
-
+import { typeDefs } from './baseSchema'
+/**
+ * Takes a graphql schema string, adds default typdefs
+ * and creates and executable schema. Takes that schema
+ * and creates a human-readable JSON structure for
+ * code gen
+ * @param {String} schema a graphql schema strong
+ * @return {Object} an object with the JSON tree for codegen
+ *                  and the compiled schema
+ */
+export const getSchemaTemplateData = schema => {
+  // convert the schema string into an AST for mergin
   const ast = parse(schema)
-  const deafultTypes = `
-    directive @ui(component: String, name: String) on FIELD
-    directive @validations(minlength: Int, maxlength: Int) on FIELD
-    directive @date(format: String) on FIELD
-    directive @type on OBJECT
-    directive @page(prodUrl: Url, previewUrl: Url) on OBJECT
 
-    scalar DateTime
-    scalar LatLong
-    scalar Url
-    
-    interface Page {
-      pageInfo: PageInfo!
-    }
-
-    type Asset {
-      url: Url!
-      name: String!
-      type: String!
-    }
-    
-    type PageInfo {
-      title: String
-      description: String
-    }
-
-    input StringFilterInput {
-      _eq: String
-      _neq: String
-      _gt: String
-      _lt: String
-      _gte: String
-      _lte: String
-      _in: [String!]
-      _nin: [String!]
-      _contains: String
-      _does_not_contain: String
-      _starts_with: String
-      _ends_with: String
-    }
-
-    input FloatFilterInput {
-      _eq: Float
-      _neq: Float
-      _gt: Float
-      _lt: Float
-      _gte: Float
-      _lte: Float
-      _in: [Float!]
-      _nin: [Float!]
-    }
-
-    input IntFilterInput {
-      _eq: Int
-      _neq: Int
-      _gt: Int
-      _lt: Int
-      _gte: Int
-      _lte: Int
-      _in: [Int!]
-      _nin: [Int!]
-    }
-
-    input DateTimeFilterInput {
-      _eq: DateTime
-      _neq: DateTime
-      _gt: DateTime
-      _lt: DateTime
-      _gte: DateTime
-      _lte: DateTime
-      _in: [DateTime!]
-      _nin: [DateTime!]
-    }
-
-    input BooleanFilterInput {
-      _eq: Boolean
-      _neq: Boolean
-    }
-
-    input IDFilterInput {
-      _eq: ID
-      _neq: ID
-    }
-
-    input UrlFilterInput {
-      _eq: Url
-      _neq: Url
-      _in: [Url!]
-      _nin: [Url!]
-      _contains: Url
-      _does_not_contain: Url
-    }
-  `
-
+  // need to convert default types into a full schema for merging with AST
   const defaultSchema = makeExecutableSchema({
-    typeDefs: [deafultTypes],
+    typeDefs: [typeDefs],
     resolvers: {
       ...scalars,
       Page: {
@@ -116,6 +27,7 @@ export const getSchemaTemplateData = () => {
     }
   })
 
+  // merge the default schema with given AST to ensure extensions work
   schema = extendSchema(defaultSchema, ast)
   const schemaTemplateData = schemaToTemplateContext(schema)
   return { schemaTemplateData, userSchema: schema }
